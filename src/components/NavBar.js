600aa1c913bcf207c3c917e8c407478e62aac4e0
Prajakta <ptprajakta21@gmail.com>
2022-09-18 19:01:07 +0530

    practiced routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
+export const NavBar = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li className='index-link'>
+                        <NavLink to={"/"}>Index</NavLink>
+                    </li>
+                    <li className="home-link">
+                        <NavLink to={"/home"}>Home</NavLink>
+                    </li>
 
-export const NavBar = () =>{
-    return(
-
+                </ul>
+            </nav>
+        </>
     )
 }
